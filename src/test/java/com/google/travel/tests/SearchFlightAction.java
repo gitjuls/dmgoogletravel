7729c6f65e7b19ca6456ac8821dd430cb5676ce6
Jules <julia.krasnitski@gmail.com>
2022-10-03 23:23:04 -0400

    "message"

--- src/test/java/com/google/travel/tests/SearchFlightAction.java
+++ src/test/java/com/google/travel/tests/SearchFlightAction.java
@@ -1,11 +1,11 @@
 package com.google.travel.tests;
 
 import com.google.travel.pages.SearchFlight;
-import com.google.travel.pages.TripOptionFactory;
-
 import java.util.function.Consumer;
 
 public class SearchFlightAction {
 
-    //public static final Consumer<SearchFlight> freeCoupon = searchFlight -> searchFlight.inputSearchData();
+    public static final Consumer<SearchFlight> oneWay = searchFlight -> searchFlight.selectTicketType("One way");
+    public static final Consumer<SearchFlight> roundTrip = searchFlight -> searchFlight.selectTicketType("Round trip");
+    public static final Consumer<SearchFlight> multiCity = searchFlight -> searchFlight.selectTicketType("Multi-city");
 }
