548c5a92006770dff9db261f23b6beb93477cc06
Jules <julia.krasnitski@gmail.com>
2022-10-11 21:44:42 -0400

    "message"

--- src/main/java/com/google/travel/pages/flightsList/FlightsList.java
+++ src/main/java/com/google/travel/pages/flightsList/FlightsList.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-
 import java.text.DecimalFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
